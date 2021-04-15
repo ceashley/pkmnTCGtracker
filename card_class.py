b6466b560e32eb19f69aa177c67ddd6cf9a2df49
Kreap <cotlonwanders@yahoo.com>
2021-04-15 16:52:17 -0600

    pokemon card class

--- card_class.py
+++ card_class.py
@@ -1,2 +1,14 @@
-def test():
-	print("banna pie")
+class PokemonCard():
+	def __init__(self, card_set, number, name, url):
+		self.card_set = card_set
+		self.number = number
+		self.name = name
+		self.url = url
+	def get_set(self):
+		return self.card_set
+	def get_name(self):
+		return self.name
+	def get_number(self):
+		return self.number
+	def get_url(self):
+		return self.url
--- pkmn_tcg_tracker.py
+++ pkmn_tcg_tracker.py
@@ -1,2 +1,10 @@
 import card_class
-card_class.test()
+
+
+#pokemon object: card set, number, name, url
+bellsprout = card_class.PokemonCard("battleStyles", 1, "bellsprout", "url")
+
+print(bellsprout.get_set())
+print(bellsprout.get_number())
+print(bellsprout.get_name())
+print(bellsprout.get_url())
