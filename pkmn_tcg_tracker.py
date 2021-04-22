72722b22273c35099937d69e3e954a342da765f8
unknown <colbyeashley@gmail.com>
2021-04-21 18:54:51 -0600

    changed imports

--- pkmn_tcg_tracker.py
+++ pkmn_tcg_tracker.py
@@ -1,4 +1,4 @@
-import card_class
+from card_class import PokemonCard
 from gui_class import gui_class
 import tkinter
 from db_class import db_class
@@ -7,7 +7,7 @@ from db_class import db_class
 bst = db_class("BST")
 #print(bst.get_set())
 
-PC = card_class.PokemonCard('BST',bst.get_set()[157])
+PC = PokemonCard('BST',bst.get_set()[157])
 #print(PC.get_url())
 
 gui_class(tkinter.Tk())
